2ef9a347f292f3e746dc33bb8a5a32deacf90541
Prajjwal Vishwakarma <94838404+PrajjwalV27@users.noreply.github.com>
2024-02-27 20:17:02 +0530

    Create Peak Element.cpp

---
+++ Peak Element.cpp
@@ -0,0 +1,19 @@
+// Peak element is defined as that element that is greater than both of its neighbors. If 'arr[i]' is the peak element, 'arr[i - 1]' < 'arr[i]' and 'arr[i + 1]' < 'arr[i]'.
+
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size();
+    if(n == 1) return 0;
+    if(arr[0]>arr[1]) return 0;
+    if(arr[n-1]>arr[n-2]) return n-1;
+
+    int low = 1, high = n-2;
+    while(low<=high) {
+        int mid = (low+high)/2;
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) {
+            return mid;
+        }
+        else if(arr[mid]>arr[mid-1]) low = mid + 1;
+        else high = mid-1;
+    }
+    return -1;
+}
