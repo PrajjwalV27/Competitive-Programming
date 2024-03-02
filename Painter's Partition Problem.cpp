865dc2f23e855d219ec5e1576a45ed50649f5c8a
Prajjwal Vishwakarma <94838404+PrajjwalV27@users.noreply.github.com>
2024-03-02 15:03:23 +0530

    Create Painter's Partition Problem.cpp

---
+++ Painter's Partition Problem.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+int countStudents(vector<int> &arr, int pages){
+    int students = 1;
+    long long pageStudents = 0;
+    for(int i=0; i<arr.size(); i++){
+        if(pageStudents + arr[i] <= pages){
+            pageStudents += arr[i];
+        }else{
+            students += 1;
+            pageStudents = arr[i];
+        }
+    }
+    return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+    if(m>n) return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while(low <= high){
+        int mid = (low+high)/2;
+        int students = countStudents(arr, mid);
+        if(students>m){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int findLargestMinDistance(vector<int> &a, int k)
+{
+    return findPages(a, a.size(), k);
+}
